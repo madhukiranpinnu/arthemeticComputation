640342ae0864b2e35764ad5458d97d0b237db696
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 16:49:51 +0530

    done first computation

--- arthemeticComputation.sh
+++ arthemeticComputation.sh
@@ -4,3 +4,8 @@ echo"welcome to arthemeticComputation"
 read number1
 read number2
 read number3
+
+##making first computation
+
+computation1=$(( $number1+$number2*$number3 ))
+echo "first computation is:" $computation1
