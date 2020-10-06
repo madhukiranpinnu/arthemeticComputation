0bca66c6fcd6e3963b556eb913a89791f48b479d
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-06 17:03:36 +0530

    usercase4 added

--- arthemeticComputation.sh
+++ arthemeticComputation.sh
@@ -12,4 +12,8 @@ echo "first computation is:" $computation1
 
 ## making second computation
 computation2=$(( $number1*$number2+$number3 ))
-echo "first computation is:" $computation2
+echo "secondt computation is:" $computation2
+
+##making third computation
+computation3=$(( $number1+$number2/$number3 ))
+echo "third computation is:" $computation3
